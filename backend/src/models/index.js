ed2a363ccdec181a8cfdaa98acbb6cca382dd329
Mandytrajano90 <mandytrajano90@gmail.com>
2024-01-18 09:10:13 -0300

    Models sheet of GET /sales

--- backend/src/models/index.js
+++ backend/src/models/index.js
@@ -1,5 +1,8 @@
 const productsModel = require('./products.model');
 
+const salesModel = require('./sales.models');
+
 module.exports = {
   productsModel,
+  salesModel,
 };
---
+++ backend/src/models/sales.models.js
@@ -0,0 +1,34 @@
+const camelize = require('camelize');
+const connection = require('./connection');
+
+const getAll = async () => {
+  const [sales] = await connection.execute(`
+  SELECT 
+    sale_id "saleId", 
+    date, 
+    product_id, 
+    quantity 
+  FROM sales_products 
+  INNER JOIN 
+    sales ON id = sale_id,`);
+  return camelize(sales);
+};
+
+const findById = async (saleId) => {
+  const [sale] = await connection.execute(`
+  SELECT 
+    date,
+    product_id,
+    quantity
+  FROM sales
+  INNER JOIN
+    sales_products ON id = sale_id
+  WHERE id = ?
+  `, [saleId]);
+  return camelize(sale);
+};
+
+module.exports = {
+  getAll,
+  findById,
+};
